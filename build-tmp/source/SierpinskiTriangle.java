53fa729298276c08b46624209c65e72abdf9140e
unknown <segottlieb@697-334-Dale.sf.k12.internal>
2015-01-23 10:33:12 -0800

    Created program

--- build-tmp/source/SierpinskiTriangle.java
+++ build-tmp/source/SierpinskiTriangle.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
 int r = (int)(Math.random()*255);
 int g = (int)(Math.random()*255);
 int b = (int)(Math.random()*255);
@@ -38,4 +54,13 @@ public void sierpinski(float x, float y, float len)
 		fill(r3,g3,b3);
 		triangle(x, y, x + len, y, x + len/2, y - len);
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
